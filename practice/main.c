d17cbcf53e2ed3b3f5f521e207202fad5d76ecf7
LEE-star-sudo <switzerland.dan@gmail.com>
2025-01-22 17:05:32 +0800

    first changes

--- practice/main.c
+++ practice/main.c
@@ -1,14 +1,9 @@
-//
-//  main.c
-//  practice
-//
-//  Created by YUEDAN LEE on 22/1/2025.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void)
+{
+    int a = 7;
+    int b = a << 1;
+    printf("%d\n",b);
     return 0;
 }
