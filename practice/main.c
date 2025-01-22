337cbdf96ecb40cc61c581451bf68eba2638fa60
LEE-star-sudo <switzerland.dan@gmail.com>
2025-01-22 17:00:52 +0800

    Initial Commit

---
+++ practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  practice
+//
+//  Created by YUEDAN LEE on 22/1/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
